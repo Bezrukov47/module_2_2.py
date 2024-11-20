8964c75e25717eb78245178a111adc58da448161
Bezrukov47 <bezr47@gmail.com>
2024-11-20 17:26:33 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+first = 16
+second = 99
+third = 99
+print (first)
+print (second)
+print (third)
+#ifirst == second == third:
+    #print ('Вывести на консоль: 3')
+#if first == second or first == third or second == third:
+    #print ('Вывести на консоль: 2')
+#if first != second and first!= third and second != third:
+    #print ('Вывести на консоль: 0')
+if first == second == third:
+    print(3)
+if first == second or first == third or second == third:
+    print(2)
+if_elif_else: first == second or first == third or second == third
+print(0)
